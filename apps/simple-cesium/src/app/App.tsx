022a6e00bfde6828d9aa39c56879503ca1707f4f
David Glogaza <david.glogaza40@googlemail.com>
2024-03-06 16:45:23 +0100

    disable ui elements

--- apps/simple-cesium/src/app/App.tsx
+++ apps/simple-cesium/src/app/App.tsx
@@ -1,7 +1,20 @@
 import { Viewer } from 'resium';
 
 export function App() {
-  return <Viewer full />;
+  return (
+    <Viewer
+      full
+      timeline={false}
+      animation={false}
+      baseLayerPicker={false}
+      homeButton={false}
+      geocoder={false}
+      navigationHelpButton={false}
+      sceneModePicker={false}
+      fullscreenButton={false}
+      infoBox={true}
+    />
+  );
 }
 
 export default App;
