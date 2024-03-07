339c208de6c41c114cd8d3285ea711ebe738fcb7
David Glogaza <david.glogaza40@googlemail.com>
2024-03-07 10:24:33 +0100

    update leaflet map layer

--- apps/simple-leaflet/src/app/Map.tsx
+++ apps/simple-leaflet/src/app/Map.tsx
@@ -1,9 +1,9 @@
 import { LatLngTuple } from 'leaflet';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { MapContainer, Marker, Popup, WMSTileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const Map = () => {
-  const position = [51.256, 7.151] as LatLngTuple;
+  const position = [51.261, 7.178] as LatLngTuple;
 
   return (
     <MapContainer
@@ -11,7 +11,14 @@ const Map = () => {
       zoom={20}
       style={{ height: 'calc(100vh - 16px)', width: '100wh' }}
     >
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <WMSTileLayer
+        url="https://maps.wuppertal.de/deegree/wms"
+        layers="R102:trueortho2022"
+        format="image/png"
+        transparent={true}
+        maxZoom={19}
+        opacity={0.7}
+      />
       <Marker position={position}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
