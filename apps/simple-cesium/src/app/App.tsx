c70c924584b724c00eb88aa98aeccdc7aa879aba
David Glogaza <david.glogaza40@googlemail.com>
2024-03-07 10:23:39 +0100

    add wupp 3d tileset to cesium example

--- apps/simple-cesium/src/app/App.tsx
+++ apps/simple-cesium/src/app/App.tsx
@@ -1,6 +1,13 @@
-import { Viewer } from 'resium';
+import { Cartesian3, Matrix4 } from 'cesium';
+import { Cesium3DTileset, Viewer } from 'resium';
 
 export function App() {
+  const heightOffset = 0;
+  const surface = Cartesian3.fromRadians(7.20009, 51.272034, 0.0);
+  const offset = Cartesian3.fromRadians(7.20009, 51.272034, heightOffset);
+  const translation = Cartesian3.subtract(offset, surface, new Cartesian3());
+  const modelMatrix = Matrix4.fromTranslation(translation);
+
   return (
     <Viewer
       full
@@ -12,8 +19,12 @@ export function App() {
       navigationHelpButton={false}
       sceneModePicker={false}
       fullscreenButton={false}
-      infoBox={true}
-    />
+    >
+      <Cesium3DTileset
+        url="https://wupp-3d-data.cismet.de/mesh/tileset.json"
+        modelMatrix={modelMatrix}
+      />
+    </Viewer>
   );
 }
 
